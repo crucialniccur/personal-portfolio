1584f249d88174d902a20610b6d1ef063996bf1a
Mati Joseph <crucialniccur@gmail.com>
2025-02-28 22:08:32 +0300

    add a function in js to make the video show onclicking a button

---
+++ script.js
@@ -0,0 +1,6 @@
+function amKidding() {
+  let video = document.getElementById("myVideo");
+  let button = document.getElementById("showVideo");
+  video.style.display = "block";
+  button.style.display = "none";
+}
